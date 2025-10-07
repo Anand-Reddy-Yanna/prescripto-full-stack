42ac4f86ad32aaf20223057d92661886523ec192
Anand-Reddy-Yanna <anandreddyyanna5@gmail.com>
2025-10-07 17:09:21 +0530

    .

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -24,8 +24,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+91 9381006725</li>
-            <li>kalyansamineni@gmail.com</li>
+            <li>+91 8688430211</li>
+            <li>anandreddyyanna5@gmail.com</li>
           </ul>
         </div>
 
